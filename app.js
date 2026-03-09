7ac855a3ce115809c03fef72ae6c4e5df4f6e85f
Nochi <255517211+Nochi12@users.noreply.github.com>
2026-03-09 15:22:10 +0800

    added a way for user to add grid
    -removed the fix h & w grid to make so
    -it changes base on user input

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const container = document.querySelector("#container")
+const button = document.querySelector(".button")
 
+createGrid(16)
 function createGrid(divs){
+    
 const numGrid = divs * divs;
+const maxGrid = 960 / divs ;
 for(let i = 0; i < numGrid ; i++)
 {
     const grids = document.createElement("div");
     grids.classList.add("grid");
-
+    grids.style.flex = `1 0  ${maxGrid + "px"}`;   
+    grids.style.border = "solid 1px  black";
+    grids.style.boxSizing = "border-Box";
     grids.addEventListener("mouseover", e => {
         e.target.style.backgroundColor = "black";
     })
@@ -15,5 +21,24 @@ for(let i = 0; i < numGrid ; i++)
 }
 }
 
+button.addEventListener("click" ,() => {
+container.innerHTML = "";
+const userInput = parseInt(prompt("Enter A number between 1 - 100 "))
 
-createGrid(16)
+if(!Number.isInteger(userInput))
+{
+    alert("User Input is not a Number!!! ")
+}
+if(userInput < 0 || userInput > 100 )
+{
+    alert("Please only put Number between 1 - 100")
+}else
+{
+return createGrid(userInput)
+    
+}
+
+})
+
+
+
